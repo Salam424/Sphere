549e9f1e3be6cf73146ac40e4aa0547d8e7e375d
UiSalam123 <abdussalamsateemae@gmail.com>
2024-02-24 17:35:35 +0700

    first-commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+
+import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
